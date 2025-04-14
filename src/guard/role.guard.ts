10a3ef767c0c5aab0233f415def115a012fcb22b
abfiker <new94abemelek@gmail.com>
2025-04-14 10:11:29 +0300

    bug fix

--- src/guard/role.guard.ts
+++ src/guard/role.guard.ts
@@ -1,5 +1,6 @@
 import { Injectable, CanActivate, ExecutionContext } from "@nestjs/common";
 import { Observable } from "rxjs";
+@Injectable()
 export class RoleGuard implements CanActivate {
     canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
         const request = context.switchToHttp().getRequest()
